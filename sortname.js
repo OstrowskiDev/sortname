f9bf21b07b293841b36dc1ebd0c1e031c961afe0
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-08-02 00:24:07 +0200

    Tested sortname in Git Bash after adding it via user/bin and dedicated commands. Added safety question using readline module with createInterface() method to prevent running the script in undesired directories by accident.

--- sortname.js
+++ sortname.js
@@ -1,11 +1,28 @@
 #!/usr/bin/env node
 
 const fs = require('fs').promises
+const readline = require('readline')
 // const path = require('path')
+
+const dirPath = process.cwd()
+
+// creates question for CLI and returns user answer
+async function askQuestion(query) {
+  const rl = readline.createInterface({
+    input: process.stdin, //read data from keyboard
+    output: process.stdout, //display in CLI terminal
+  })
+
+  return new Promise((resolve) => {
+    rl.question(query, (answer) => {
+      rl.close()
+      resolve(answer.trim().toLowerCase())
+    })
+  })
+}
 
 async function getFiles() {
   try {
-    const dirPath = process.cwd()
     const files = await fs.readdir(dirPath)
     return files
   } catch (error) {
@@ -15,6 +32,13 @@ async function getFiles() {
 }
 
 async function main() {
+  const answer = await askQuestion(`Are you sure you want to change filenames in: '${dirPath}' ? (y/n): `)
+
+  if (answer !== 'y') {
+    console.log(`sortname aborted by user`)
+    process.exit(0)
+  }
+
   const files = await getFiles()
   if (files.length === 0) {
     console.log('No files in directory')
