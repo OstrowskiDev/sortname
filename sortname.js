c9a98a509f96985a7f9e207604fa7881ba8a3175
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-08-02 02:02:18 +0200

    Added rest of MVP logic to sortname.js Now it reads all files of dir it is executed from. Excludes folders, sorts files by creation time, and renames them numerically starting at 001 or 0001 (for numbers in range 999-9999). Script will stop working if number of files exceeds 9999. Time for tests in Git Bash.

--- sortname.js
+++ sortname.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs').promises
 const readline = require('readline')
-// const path = require('path')
+const path = require('path')
 
 const dirPath = process.cwd()
 
@@ -23,12 +23,60 @@ async function askQuestion(query) {
 
 async function getFiles() {
   try {
-    const files = await fs.readdir(dirPath)
-    return files
+    const files = await fs.readdir(dirPath, { withFileTypes: true })
+    let data = []
+
+    for (const file of files) {
+      if (!file.isFile()) continue
+
+      const fullPath = path.join(dirPath, file.name)
+      const ext = path.extname(file.name)
+      const baseName = path.basename(file.name, ext)
+      const stats = await fs.stat(fullPath)
+      const createdAt = stats.birthtime.getTime() // returns number in ms
+      const fileData = {
+        fullPath,
+        ext,
+        baseName,
+        createdAt,
+      }
+
+      data.push(fileData)
+    }
+
+    return data
   } catch (error) {
     console.error('Error reading directory:', error)
     return []
   }
+}
+
+function sortFiles(files) {
+  return files.sort((a, b) => a.createdAt - b.createdAt)
+}
+
+async function renameFiles(files) {
+  let count = 1
+  for (const file of files) {
+    const newBaseName = createName(count)
+    if (newBaseName === 'error') {
+      throw new Error('Too many files in directory, sortname does not support more than 9999 files in single directory')
+    }
+    const oldName = path.join(dirPath, file.baseName + file.ext)
+    const newName = path.join(dirPath, newBaseName + file.ext)
+    await fs.rename(oldName, newName)
+    count++
+  }
+}
+
+function createName(number) {
+  if (number > 9999) {
+    return 'error'
+  }
+  if (number > 999) {
+    return String(number).padStart(4, '0')
+  }
+  return String(number).padStart(3, '0')
 }
 
 async function main() {
@@ -45,8 +93,14 @@ async function main() {
     return
   }
 
-  console.log('Files found in directory:', files)
-  //dalsze działania na files
+  const sorted = sortFiles(files)
+
+  try {
+    await renameFiles(sorted)
+    console.log('Files renamed successfully')
+  } catch (err) {
+    console.error(err.message)
+  }
 }
 
 main()
